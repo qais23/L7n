87794387385910f7c345c88fbb3306deb9d90339
qais23 <55105097+qais23@users.noreply.github.com>
2019-09-09 12:44:50 -0500

    l7n

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page('public/index.html')
+client_scripts {
+    'client.lua',
+}
+files({
+    'public/css/hcdd.ttf',
+    'public/css/bootstrap.min.css',
+    'public/js/bootstrap.min.js',
+    'public/css/style.css',
+    'public/index.html',
+    'public/js/config.js',
+    'public/js/main.js'
+})
