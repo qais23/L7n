7070ef50a4008269bbd3830f8abee79c383595b4
qais23 <55105097+qais23@users.noreply.github.com>
2019-09-09 12:48:14 -0500

    l7n

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+    //Banner settings
+    banner: {
+        fadeTime: 'slow', //can be slow, fast or the ammount of milliseconds
+        interval: 3,
+        messages: [
+            "Arab The ^8 BEST Be L7n <span></span>",
+            "<span class=\"text-info\">Arab The Best</span> ",
+            "/<span class=\"text-success\">discord.gg/zvBft8</span>",
+        ]
+    }
+}
